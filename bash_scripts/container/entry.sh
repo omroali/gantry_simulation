52d06fb81e2268e00d194e391df8f8ab8e81665a
Omro <49950899+omroali@users.noreply.github.com>
2025-11-11 23:38:14 +0000

    adding submodules rviz_satellite and topological navigation

--- bash_scripts/container/entry.sh
+++ bash_scripts/container/entry.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 set -e
 # Set ROS network configuration for host network mode
-echo export ROS_IP="127.0.0.1" >> .bashrc
-echo export ROS_HOSTNAME="localhost" >> .bashrc
-echo export DISABLE_ROS1_EOL_WARNINGS="True"  >> .bashrc
-echo source "/opt/ros/noetic/setup.bash"  >> .bashrc
+echo export ROS_IP="127.0.0.1" >> /home/${USERNAME}/.bashrc
+echo export ROS_HOSTNAME="localhost" >> /home/${USERNAME}/.bashrc
+echo export DISABLE_ROS1_EOL_WARNINGS="True"  >> /home/${USERNAME}/.bashrc
+echo source "/opt/ros/noetic/setup.bash"  >> /home/${USERNAME}/.bashrc
+source "/opt/ros/noetic/setup.bash"
+cd "/home/ros/catkin_ws"
+catkin_make
+echo source "/home/ros/catkin_ws/devel/setup.bash" >> /home/${USERNAME}/.bashrc
+cd "/home/ros"
 source "/home/${USERNAME}/.bashrc"
 exec "$@"
 
